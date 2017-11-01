10abc4d13703ef9fa75c745ad791d94813980ae6
Mauricio Manoel <mauriciomanoel@gmail.com>
2017-11-01 09:14:20 -0300

    Add atividade 4

---
+++ iaatividade4/src/com/mauricio/CharacterReader.java
@@ -0,0 +1,31 @@
+package com.mauricio;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class CharacterReader {
+
+	public static void readImage() throws IOException {
+
+		BufferedImage image = ImageIO.read(new File("C:\\projetos\\desktop\\pgeeIA2017.2\\iaatividade4\\fotos\\pessoa4_ajustado.jpg"));
+		byte[][] pixels = new byte[image.getWidth()][];
+		System.out.print("new int[] {");
+		for (int x = 0; x < image.getWidth(); x++) {
+			pixels[x] = new byte[image.getHeight()];			
+			
+			for (int y = 0; y < image.getHeight(); y++) {
+				pixels[x][y] = (byte) (image.getRGB(x, y) == 0xFFFFFFFF ? 0 : 1);
+				System.out.print(pixels[x][y]+",");
+			}
+			
+			
+		}System.out.print("},");
+	}
+	
+	public static void main(String[] args) throws IOException {
+		readImage();
+	}
+}
---
+++ iaatividade4/src/com/mauricio/Constants.java
@@ -0,0 +1,13 @@
+package com.mauricio;
+
+public class Constants {
+
+	private Constants() {
+		
+	}
+	
+	public static final float LEARNING_RATE = 0.3f;
+	public static final float MOMENTUM = 0.6f;
+	
+	public static final int ITERATIONS = 10000;
+}
